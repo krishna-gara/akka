5da08a2fe5afeb392c93e2e0261a41fd0a27ca1b
krishna-gara <krishna.gara@compugain.com>
2018-04-18 16:07:21 +0530

    minor changes in error handling class

--- src/main/scala/HttpClientExampleApp.scala
+++ src/main/scala/HttpClientExampleApp.scala
@@ -9,30 +9,30 @@ import scala.concurrent.Future
 // you must import the DSL to use the syntax helpers
 import com.sksamuel.elastic4s.http.ElasticDsl._
 
+//# extending the error handling class to current method
+object HttpClientExampleApp extends ErrorHandling with App {
+    val config = ConfigFactory.load()
+    this.pusherror(config.getString("elasticsearch.hostname"),config.getString("elasticsearch.port"),"DbEorr","[error] [C:\\\\Users\\\\kgara\\\\COE\\\\Akka\\\\akka-examples\\\\build.sbt]:17: identifier expected but something found.")
+}
 
-object HttpClientExampleApp extends App {
-
-
-  val config = ConfigFactory.load()
-  val client = HttpClient(ElasticsearchClientUri(config.getString("elasticsearch.hostname"), config.getInt("elasticsearch.port")))
-
-  client.execute {
-    bulk(
-      //indexInto("myindex" / "mytype").fields("country" -> "Mongolia", "capital" -> "Ulaanbaatar"),
-      //indexInto("myindex" / "mytype").fields("country" -> "Namibia", "capital" -> "Windhoek"),
-      //indexInto("myindex" / "mytype").fields("country" -> "India", "capital" -> "Delhi"),
-      //indexInto("myindex" / "mytype1").fields("country" -> "India", "capital" -> "Delhi"),
-      indexInto("custom_errors" / "mytype1").fields("type" -> "AppError", "message" -> "[error] [C:\\Users\\kgara\\COE\\Akka\\akka-examples\\build.sbt]:17: identifier expected but something found.")
-    ).refresh(RefreshPolicy.WAIT_UNTIL)
-  }.await
-
-  val result: SearchResponse = client.execute {
-    search("myindex1").matchQuery("capital", "Delhi")
-  }.await
-
-  val resp = client.execute { search("custom_errors") matchQuery("message","expected") }.await
-  println(resp.hits.hits foreach println )
-  //prints out the original json
-
-  client.close()
+class ErrorHandling {
+  //# method to push all data to elasticsearch
+  def pusherror(ipAddres:String,port:String,errtype:String,message:String):String = {
+    //# elasticsearch connection
+    val client = HttpClient(ElasticsearchClientUri(ipAddres, port.toInt))
+    try{
+      //# elasticsearch insertion
+      client.execute {
+        bulk(
+          indexInto("custom_errors" / "mytype1").fields("type" -> errtype, "message" -> message)
+        ).refresh(RefreshPolicy.WAIT_UNTIL)
+      }.await
+      "Pushed to database"
+    }catch{
+      case e: Exception => e.printStackTrace()
+      "Not pushed to database"
+    } finally {
+      client.close()
+    }
+  }
 }
